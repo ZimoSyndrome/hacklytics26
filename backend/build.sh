8119cb214fd58dde9b7b4d19e6c99b39ef581d75
feliciahmq <feliciahmq@gmail.com>
2026-02-22 02:36:54 -0500

    ppp

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip install --upgrade pip
-pip install -i https://download.pytorch.org/whl/cpu torch==2.10.0
-pip install flask flask-cors lightgbm numpy scikit-learn joblib sentence-transformers pdfplumber
+uv pip install --upgrade pip
+uv pip install -i https://download.pytorch.org/whl/cpu torch==2.10.0
+uv pip install flask flask-cors lightgbm numpy scikit-learn joblib sentence-transformers pdfplumber
