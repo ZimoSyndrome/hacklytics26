aa78970c0b39267e8cf2341d41121304e58c6753
feliciahmq <feliciahmq@gmail.com>
2026-02-22 02:42:15 -0500

    lll

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source .venv/bin/activate
 uv pip install --upgrade pip
 uv pip install -i https://download.pytorch.org/whl/cpu torch==2.10.0
 uv pip install flask flask-cors lightgbm numpy scikit-learn joblib sentence-transformers pdfplumber
